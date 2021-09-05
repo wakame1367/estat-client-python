4485c9d5675022fed2e2230002a55b1c4e387b29
wakamezake <hotwater1367@gmail.com>
2021-09-05 17:18:55 +0900

    add base_url

--- estat/constants.py
+++ estat/constants.py
@@ -1,1 +1,6 @@
+from urllib.parse import urljoin
+
 ESTAT_BASE_URL = 'http://api.e-stat.go.jp'
+API_VERSION = '3.0'
+BASE_PATH = f'/rest/{API_VERSION}/app/'
+BASE_URL = urljoin(ESTAT_BASE_URL, BASE_PATH)
